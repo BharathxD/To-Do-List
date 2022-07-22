463249b7ee1722c9d926edf7eeb92b983c8e9cff
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-07-22 22:25:11 +0530

    Initial release

--- app.js
+++ app.js
@@ -109,5 +109,5 @@ app.post("/delete", function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port 3000");
 });
