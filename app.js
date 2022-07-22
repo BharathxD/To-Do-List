a421df78bf231e8ca0ee71dd4c677476d8254cc4
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-07-22 21:38:41 +0530

    Reworking on JSON

--- app.js
+++ app.js
@@ -110,13 +110,6 @@ app.post("/delete", function (req, res) {
   });  
 });
 
-let port = process.env.PORT;
-if (port == null || port == "" ){
-  port = 3000;
-}
-
-app.listen(port, function (err) {
-  if(err){
-  console.log(err);
-  }
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
