6f005838bb9f08a8e96726d1ef2c750d9703e631
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-07-22 20:43:47 +0530

    Update Port and add gitignore

--- app.js
+++ app.js
@@ -110,7 +110,12 @@ app.post("/delete", function (req, res) {
   });  
 });
 
-app.listen(process.env.PORT || 3000, function (err) {
+let port = process.env.PORT;
+if (port == null || port == "" ){
+  port = 3000;
+}
+
+app.listen(port, function (err) {
   if(err){
   console.log(err);
   }
